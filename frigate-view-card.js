478df2ff332407365877ec42f9f6132bb0c5a4f9
htpchome <117685139+htpchome@users.noreply.github.com>
2026-07-16 23:49:00 +0000

    169

--- frigate-view-card.js
+++ frigate-view-card.js
@@ -7,7 +7,7 @@
  * ---------------------------------------------------------------
  */
 
-const VERSION = "1.0.168";
+const VERSION = "1.0.169";
 
 const CARD_TAG = "frigate-view-card";
 const DAY = 86400;
