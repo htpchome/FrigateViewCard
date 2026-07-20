afe3b4605cd530ebb0ce4bc7e6024e6287b05e76
htpchome <117685139+htpchome@users.noreply.github.com>
2026-07-20 23:41:22 +0000

    538

--- frigate-view-card.js
+++ frigate-view-card.js
@@ -1,7 +1,7 @@
 /** FrigateView Card - generated file. Edit src/ instead. */
 
 // src/constants.js
-const VERSION = "1.0.537";
+const VERSION = "1.0.538";
 const CARD_TAG = "frigate-view-card";
 const DAY = 86400;
 const RECORDINGS_WINDOW = 24 * 3600;
