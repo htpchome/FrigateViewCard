fc587fa3335f6d735fc1fd8256b29a5ddf0016bd
htpchome <117685139+htpchome@users.noreply.github.com>
2026-08-04 21:25:50 +0000

    1174

--- frigate-view-card.js
+++ frigate-view-card.js
@@ -4,7 +4,7 @@ const __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { 
 const __publicField = (obj, key, value) => __defNormalProp(obj, typeof key !== "symbol" ? key + "" : key, value);
 
 // src/constants.js
-const VERSION = "1.0.1173";
+const VERSION = "1.0.1174";
 const CARD_TAG = "frigate-view-card";
 const DAY = 86400;
 const RECORDINGS_WINDOW = 24 * 3600;
