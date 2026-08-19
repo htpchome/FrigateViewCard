67bc807a0116c0fe053ac9ea099d785c1b06dfea
htpchome <117685139+htpchome@users.noreply.github.com>
2026-08-19 21:47:43 +0000

    1533

--- src/styles.js
+++ src/styles.js
@@ -367,8 +367,6 @@ export const STYLES = `
   .square-btn:hover{background: rgb(45 45 45 / 95%);border-color: rgb(255 255 255 / 45%);}
   .square-btn svg{width: 24px;height: 24px;fill: currentColor;pointer-events: none;}
   .mute-btn {position:absolute;right:20px;bottom:32px;z-index:3;width:36px;height:36px;opacity:0;pointer-events:none;transition:opacity .16s ease;}
-  .live-fs-btn{position:absolute;right:20px;bottom:84px;z-index:3;width:36px;height:36px;opacity:0;pointer-events:none;transition:opacity .16s ease;}
-  .pip-btn{}
 
   .live-playback-controls{position:absolute;right:20px;bottom:84px;z-index:3;display:flex;gap:4px;opacity:0;pointer-events:none;transition:opacity .16s ease;}
   .live-playback-controls .live-playback-btn{position:relative;width:36px;height:36px;}
